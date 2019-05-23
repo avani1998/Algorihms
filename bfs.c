0455f00a4b64b12edd0b4757ce2d0af5e65d62d3
avani1998 <46353367+avani1998@users.noreply.github.com>
2019-05-23 15:31:02 +0530

    Update bfs.c

--- bfs.c
+++ bfs.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int count=0;
-void bfs(int source,int n,int a[20][20],int visited[20])
+void bfs(int a[20][20],int n,int visited[],int source)
 {
     int queue[20],f,r,u,v;
     f=0;r=-1;
@@ -14,46 +14,33 @@ void bfs(int source,int n,int a[20][20],int visited[20])
         {
             if(a[u][v]==1 && visited[v]==0)
             {
-                queue[++r]=v;
                 count++;
                 visited[v]=count;
+                queue[++r]=v;
             }
         }
     }
 }
-
 void main()
 {
-    int source,a[20][20],visited[20];int n;int i,j;int visitorder[20];
-    printf("Enter the number of vertices\n");
+    int i,j,n,a[20][20],visited[20],source,visitorder[20];
+    printf("Enter the number of elements\n");
     scanf("%d",&n);
+    printf("Enter adjacency matrix\n");
+    for(i=1;i<=n;i++)
+        for(j=1;j<=n;j++)
+            scanf("%d",&a[i][j]);
+    for(i=1;i<=n;i++)
+        visited[i]=0;
     printf("Enter the source\n");
     scanf("%d",&source);
-    printf("Enter the adjacency matrix\n");
+    bfs(a,n,visited,source);
     for(i=1;i<=n;i++)
-    {
-        for(j=1;j<=n;j++)
-        {
-            scanf("%d",&a[i][j]);
-        }
-    }
-    for(i=1;i<=n;i++)
-    visited[i]=0;
-    bfs(source,n,a,visited);
-    for(i=1;i<=n;i++)
-    {
         if(visited[i]==0)
-        {
-            bfs(source,n,a,visited);
-        }
-    }
-    for(i=0;i<=n;i++)
-    {
-        visitorder[visited[i]]=i;
-    }
-    printf("The bfs is\n");
+            bfs(a,n,visited,i);
     for(i=1;i<=n;i++)
-    {
+        visitorder[visited[i]]=i;
+    printf("The bfs traversal is\n");
+    for(i=1;i<=n;i++)
         printf("%c\t",visitorder[i]+64);
-    }
 }
