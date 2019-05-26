4c7c448d517920b2ea7a714a85600de3b6ec7d42
avani1998 <46353367+avani1998@users.noreply.github.com>
2019-05-26 17:59:18 +0530

    Update Merge Sort.c

--- Merge Sort.c
+++ Merge Sort.c
@@ -1,54 +1,52 @@
 #include<stdio.h>
-void main()
-{
-	int a[30],n,i;
-	printf("Enter no of elements:");
-	scanf("%d",&n);
-	printf("Enter array elements:");
-	
-	for(i=0;i<n;i++)
-		scanf("%d",&a[i]);
-		
-	mergesort(a,0,n-1);
-	
-	printf("\nSorted array is :");
-	for(i=0;i<n;i++)
-		printf("%d ",a[i]);
-}
- 
-void mergesort(int a[],int low,int high)
-{
-	int mid;
-		
-	if(low<high)
-	{
-		mid=(low+high)/2;
-		mergesort(a,low,mid);		//left recursion
-		mergesort(a,mid+1,high);	//right recursion
-		merge(a,low,mid+1,high);	//merging of two sorted sub-arrays
-	}
-}
- 
 void merge(int a[],int low,int mid,int high)
 {
-	int b[50];
-	int i=low,j=mid,k;
-	k=0;
-	
-	while(i<=mid && j<=high)	//while elements in both lists
-	{
-		if(a[i]<a[j])
-			b[k++]=a[i++];
-		else
-			b[k++]=a[j++];
-	}
-	
-	while(i<=mid)	//copy remaining elements of the first array
-		b[k++]=a[i++];
-		
-	while(j<=high)	//copy remaining elements of the second array
-		b[k++]=a[j++];
-		
-	for(i=low,j=0;i<=high;i++,j++)
-		a[i]=b[j];
+    int l1,l2,i,b[100];
+    for(l1=low,l2=mid,i=low;l1<=mid && l2<=high;i++)
+    {
+        if(a[l1]<a[l2])
+        b[i]=a[l1++];
+        else
+        b[i]=a[l2++];
+    }
+    while(l1<=mid)
+    b[i++]=a[l1++];
+    while(l2<=high)
+    b[i++]=a[l2++];
+    for(i=low;i<=high;i++)
+    a[i]=b[i];
 }
+void mergesort(int a[],int low,int high)
+{
+    int mid;
+    if(low<high)
+    {
+        mid=(low+high)/2;
+        mergesort(a,low,mid);
+        mergesort(a,mid+1,high);
+        merge(a,low,mid+1,high);
+    }
+}
+void main()
+{
+    int a[100],i,j,n;
+    printf("Enter the number of elements\n");
+    scanf("%d",&n);
+    printf("Enter the array\n");
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+    mergesort(a,0,n-1);
+    printf("The sorted array is \n");
+    for(i=0;i<n;i++)
+    printf("%d\t",a[i]);
+}
+/*Enter the number of elements                                                                                                                     
+5                                                                                                                                                
+Enter the array                                                                                                                                  
+6                                                                                                                                                
+3                                                                                                                                                
+9                                                                                                                                                
+7                                                                                                                                                
+1                                                                                                                                                
+The sorted array is                                                                                                                              
+1       3       6       7       9   */
