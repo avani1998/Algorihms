e4b4ea4916785993648f907bfaad3a7031b9f704
avani1998 <46353367+avani1998@users.noreply.github.com>
2019-05-26 19:07:46 +0530

    Update Quick Sort.c

--- Quick Sort.c
+++ Quick Sort.c
@@ -1,5 +1,46 @@
 #include<stdio.h>
 #include<time.h>
+int partition(int A[],int low,int high)
+
+{
+
+    int i,j;
+
+    i=low;
+
+    j=high;
+    int p=A[low];
+    while(i<j)
+    {
+        while(A[i]<=p&& i<high)
+        {
+            i++;
+        }
+        while(A[j]>p && j>low)
+        {
+            j--;
+        }
+        if(i<j)
+        {
+            int temp=A[i];
+            A[i]=A[j];
+            A[j]=temp;
+        }
+    }
+    A[low]=A[j];
+    A[j]=p;
+    return j;
+}
+void quicksort(int A[],int low,int high)
+{
+    int j;
+    if(low<high)
+    {
+        j=partition(A,low,high);
+        quicksort(A,low,j-1);
+        quicksort(A,j+1,high);
+    }
+}
 void main()
 {
     int A[100000],i,j,n;
@@ -21,41 +62,4 @@ void main()
         printf("%d\t",A[i]);
     }
     printf("Time taken to sort %d elements is %f\n",n,time);
-}
-void quicksort(int A[],int low,int high)
-{
-    int j;
-    if(low<high)
-    {
-        j=partition(A,low,high);
-        quicksort(A,low,j-1);
-        quicksort(A,j+1,high);
-    }
-}
-
-void partition(int A[],int low,int high)
-{
-    int i,j;
-    i=low;
-    j=high;
-    int p=A[low];
-    while(i<j)
-    {
-        while(A[i]<=p&& i<high)
-        {
-            i++;
-        }
-        while(A[j]>=p&&j>low)
-        {
-            j--;
-        }
-        if(i<j)
-        {
-            int temp=A[i];
-            A[i]=A[j];
-            A[j]=temp;
-        }
-    A[low]=A[j];
-    A[j]=p;
-    }
 }
